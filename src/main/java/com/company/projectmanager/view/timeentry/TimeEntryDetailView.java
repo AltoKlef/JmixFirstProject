07ea12ebc74da40bda9478fff69815bb92f658f6
Ruslan <garridubua@gmail.com>
2025-04-15 14:04:02 +0300

    added localTime to TimeEntryDetailView

--- src/main/java/com/company/projectmanager/view/timeentry/TimeEntryDetailView.java
+++ src/main/java/com/company/projectmanager/view/timeentry/TimeEntryDetailView.java
@@ -5,15 +5,20 @@ import com.company.projectmanager.entity.TaskPriority;
 import com.company.projectmanager.entity.TimeEntry;
 import com.company.projectmanager.view.main.MainView;
 import com.vaadin.flow.router.Route;
+import io.jmix.core.TimeSource;
 import io.jmix.flowui.view.*;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Route(value = "time-entries/:id", layout = MainView.class)
 @ViewController(id = "TimeEntry.detail")
 @ViewDescriptor(path = "time-entry-detail-view.xml")
 @EditedEntityContainer("timeEntryDc")
 public class TimeEntryDetailView extends StandardDetailView<TimeEntry> {
+    @Autowired
+    private TimeSource timeSource;
+
     @Subscribe
     public void onInitEntity(final InitEntityEvent<TimeEntry> event) {
-        event.getEntity().setEntryDate(LocalDate.now().atStartOfDay());
+        event.getEntity().setEntryDate(timeSource.now().toLocalDateTime());
     }
 }
