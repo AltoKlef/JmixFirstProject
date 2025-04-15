b251f71fb6adfb843aa922d6082b9003337eb5c3
Ruslan <garridubua@gmail.com>
2025-04-15 13:15:13 +0300

    added avatar and fileUpload

--- src/main/java/com/company/projectmanager/view/projectstats/ProjectStatsListView.java
+++ src/main/java/com/company/projectmanager/view/projectstats/ProjectStatsListView.java
@@ -1,11 +1,14 @@
 package com.company.projectmanager.view.projectstats;
 
+import com.company.projectmanager.app.ProjectStatsService;
 import com.company.projectmanager.entity.ProjectStats;
 import com.company.projectmanager.view.main.MainView;
 import com.vaadin.flow.router.Route;
 import io.jmix.core.LoadContext;
 import io.jmix.flowui.view.*;
+import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.Collection;
 import java.util.List;
 
 @Route(value = "project-statses", layout = MainView.class)
@@ -15,10 +18,13 @@ import java.util.List;
 @DialogMode(width = "50em")
 public class ProjectStatsListView extends StandardListView<ProjectStats> {
 
+    @Autowired
+    private ProjectStatsService projectStatsService;
+
     @Install(to = "projectStatsesDl", target = Target.DATA_LOADER)
     protected List<ProjectStats> projectStatsesDlLoadDelegate(LoadContext<ProjectStats> loadContext) {
         // Here you can load entities from an external storage.
         // Set the loaded entities to the not-new state using EntityStates.setNew(entity, false).
-        return List.of();
+        return projectStatsService.fetchProjectStatistics();
     }
 }
